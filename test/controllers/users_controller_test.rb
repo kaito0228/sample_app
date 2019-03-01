c133009b5e80c0f578c639495a49f5a605d551ed
ryo.s <aqua.timez.saiko@gmail.com>
2019-03-01 16:34:45 +0900

    use ajax

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
-    @other_user= users(:archer)
+    @other_user = users(:archer)
   end
 
   test "should redirect index when not logged in" do
@@ -27,15 +27,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     log_in_as(@other_user)
     assert_not @other_user.admin?
     patch user_path(@other_user), params: {
-      user: { password:              @other_user.password,
-              password_confirmation: @other_user.password,
-              admin: true } }
+      user: {password: @other_user.password,
+             password_confirmation: @other_user.password,
+             admin: true}}
     assert_not @other_user.reload.admin?
   end
 
   test "should redirect update when not logged in" do
-    patch user_path(@user), params: { user: { name: @user.name,
-                                              email: @user.email } }
+    patch user_path(@user), params: {user: {name: @user.name,
+                                            email: @user.email}}
     assert_not flash.empty?
     assert_redirected_to login_url
   end
@@ -49,8 +49,8 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should redirect update when logged in as wrong user" do
     log_in_as(@other_user)
-    patch user_path(@user), params: { user: { name: @user.name,
-                                              email: @user.email } }
+    patch user_path(@user), params: {user: {name: @user.name,
+                                            email: @user.email}}
     assert flash.empty?
     assert_redirected_to root_url
   end
@@ -68,5 +68,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       delete user_path(@user)
     end
     assert_redirected_to root_url
+  end
+
+  test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
   end
 end
